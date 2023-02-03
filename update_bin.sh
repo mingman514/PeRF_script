6bc9cb462d7afed11dc9ad79b2b138c3cf53b50f
mingman <mingman514>
2023-02-03 07:18:25 +0000

    MODIFED scripts

---
+++ update_bin.sh
@@ -0,0 +1,19 @@
+CUR=$(pwd)
+#cp ../perf/perftest-multhrd/ib_* $CUR/bin
+cd ../perf/perftest-4.5.0-multhrd
+./build.sh
+cp ./ib_* $CUR/bin
+
+if [ $MODE -eq 1 ]; then
+pkill pacer
+  cd ../Justitia/rdma_pacer
+  make clean && make
+  cp pacer $CUR/bin
+fi
+
+if [ $MODE -eq 2 ]; then
+  pkill perf_main
+  cd $CUR/../perf/perf_src
+  gcc perf_main.c -o perf_main -lpthread -lrt
+  cp perf_main $CUR/bin
+fi
