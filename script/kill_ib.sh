cbe3372ce575e314c38b78c16e6101837b8cb9b5
mingman <mingyuchoi514@gmail.com>
2024-01-11 03:54:41 +0000

    FIX typo & added echo

--- script/kill_ib.sh
+++ script/kill_ib.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # PERF_ENABLED가 정의되어 있지 않거나 0이면 "-TERM", 1이면 "-INT"를 나타내는 SIGNAL 변수 생성
-if [ -z "$PERF_ENABLED" ] || [ "$PERF_ENABLED" -eq 0 ]; then
+if [ -z "$PERF_ENABLE" ] || [ "$PERF_ENABLE" -eq 0 ]; then
+    echo "DEFAULT/Justitia Mode"
     SIGNAL="-TERM"
 else
+    echo "PERF Mode"
     SIGNAL="-INT"
 fi
 
